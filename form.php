9e5842812427e70e1e0b31665d6e3765e91b28cc
-v <mark.selyaeff@yandex.ru>
2016-12-07 22:05:19 +0300

    initial commit

---
+++ form.php
@@ -0,0 +1,57 @@
+<?
+// Страница регистрации нового пользователя
+
+# Соединямся с БД
+$link=mysqli_connect("localhost", "root", "1997", "mysite");
+
+if(isset($_POST['submit']))
+{
+    $err = array();
+
+    # проверям логин
+    if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login']))
+    {
+        $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+    }
+
+    if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30)
+    {
+        $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+    }
+
+    # проверяем, не сущестует ли пользователя с таким именем
+    $query = mysqli_query($link, "SELECT COUNT(user_id) as total FROM users WHERE user_login='".mysqli_real_escape_string($link, $_POST['login'])."'");
+    $row = mysqli_fetch_array($query);
+    if($row[0] > 0)
+    {
+        $err[] = "Пользователь с таким логином уже существует в базе данных";
+    }
+
+    # Если нет ошибок, то добавляем в БД нового пользователя
+    if(count($err) == 0)
+    {
+
+        $login = $_POST['login'];
+
+        # Убераем лишние пробелы и делаем двойное шифрование
+        $password = md5(md5(trim($_POST['password'])));
+
+        mysqli_query($link,"INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
+        header("Location: login.php"); exit();
+    }
+    else
+    {
+        print "<b>При регистрации произошли следующие ошибки:</b><br>";
+        foreach($err AS $error)
+        {
+            print $error."<br>";
+        }
+    }
+}
+?>
+
+<form method="POST">
+    <label for="login">Логин</label> <input name="login" type="text" id="login"><br>
+    <label for="password">Пароль</label> <input name="password" type="password" id="password"><br>
+    <input name="submit" type="submit" value="Зарегистрироваться">
+</form>
